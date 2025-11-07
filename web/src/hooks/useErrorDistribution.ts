5fb3baab203c73b25f8bf4251f4d12f5aaa21af0
Ivan Li <ivanli2048@gmail.com>
2025-11-07 20:16:52 +0800

    chore(web): remove debug logs to satisfy eslint
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- web/src/hooks/useErrorDistribution.ts
+++ web/src/hooks/useErrorDistribution.ts
@@ -9,12 +9,15 @@ export function useErrorDistribution(range: string, top?: number) {
 
   const load = useCallback(async () => {
     setIsLoading(true)
+    // console.debug('[useErrorDistribution] load start', { range, top })
     try {
       const res = await fetchErrorDistribution(range, top != null ? { top } : undefined)
       setData(res)
       setError(null)
+      // console.debug('[useErrorDistribution] load ok', res.items?.length)
     } catch (err) {
       setError(err instanceof Error ? err.message : String(err))
+      // console.debug('[useErrorDistribution] load error', err)
     } finally {
       setIsLoading(false)
     }
