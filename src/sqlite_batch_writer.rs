32650cf229fcb3af451c18db20b0c6a7297453b9
Ivan Li <ivanli2048@gmail.com>
2026-08-18 18:38:51 +0800

    test: widen sqlite flush observation window
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- src/sqlite_batch_writer.rs
+++ src/sqlite_batch_writer.rs
@@ -5166,7 +5166,7 @@ mod tests {
             })
         };
 
-        let resumed = tokio::time::timeout(Duration::from_secs(3), async {
+        let resumed = tokio::time::timeout(Duration::from_secs(10), async {
             loop {
                 if writer.accounting_snapshot().p2_flush_attempt_count > 0 {
                     break;
